2b82be1ed4e722042191630e710dfeef0ea5e06e
GeraZorzoni <gzorzoni@gmail.com>
2023-10-09 15:14:33 -0300

    Trabajo sobre armado de carpetas y archivos index

---
+++ src/data/users/users.js
@@ -0,0 +1,35 @@
+// USERS - CREATE SAVE MODIFY DELETE ID
+const fs = require("fs");
+const path = require("path");
+
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = {
+  getUsers: function () {
+    const usersFilePath = path.join(__dirname, "./usersDataBase.json");
+    const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+    return users;
+  },
+
+  saveUsers: function (users) {
+    const usersFilePath = path.join(__dirname, "./usersDataBase.json");
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2)); // escribo la base de datos
+    return users;
+  },
+
+  showAll: function () {
+    return this.getUsers();
+  },
+
+  create: function (user) {
+    // aca crea el producto
+    console.log(`Creating user ${user.first_name}`);
+    const users = this.getUsers(); // traigo todos los Usuarios
+    const newUser = {
+      id: uuidv4(),
+      ...user,
+    };
+    users.push(newUser); // push con los nuevos datos
+    this.saveUsers(users); // Graba Usuario nuevo
+  },
+};
